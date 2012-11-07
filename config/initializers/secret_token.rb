eca138635314f7babe5080e5a5c4d05c41b9b952
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2012-11-07 14:38:51 +0530

    Blank skeleton app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TbTestapp::Application.config.secret_token = 'd75283fedff325bb0c636535af34aaec6531a9f41c7df2c569a5a822c316162665ca83bb2ade36e1e8792a514e7580f314f4d1af51482d2a43b8f3aa162eb5b4'
